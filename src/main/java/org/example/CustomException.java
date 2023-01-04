9977205fc9b479b93b9964195a6ff863ecea9131
PriyankaShinde125 <pkshinde125@gmail.com>
2023-01-04 16:47:50 +0530

    ADDED : Main to analyser to load csv data

--- src/main/java/org/example/CustomException.java
+++ src/main/java/org/example/CustomException.java
@@ -4,4 +4,8 @@ public class CustomException extends Exception {
     public CustomException(ExceptionType type) {
         super(type.getMessage());
     }
+
+    public CustomException(String message) {
+        super(message);
+    }
 }
--- src/main/java/org/example/StateCensusAnalyser.java
+++ src/main/java/org/example/StateCensusAnalyser.java
@@ -10,6 +10,11 @@ import java.io.IOException;
 import java.util.List;
 
 public class StateCensusAnalyser {
+    public static void main(String[] args) throws CustomException {
+        List<CsvStateCensus> data = new StateCensusAnalyser().loadData("IndiaStateCensusData.csv");
+        data.forEach(System.out::println);
+    }
+
     public List<CsvStateCensus> loadData(String fileName) throws CustomException {
         if (!fileName.endsWith(".csv"))
             throw new CustomException(ExceptionType.INVALID_FILE_TYPE);
@@ -20,10 +25,9 @@ public class StateCensusAnalyser {
                     .withType(CsvStateCensus.class)
                     .build()
                     .parse();
-            csvStateCensusData.forEach(System.out::println);
         } catch (IOException e) {
             throw new CustomException(ExceptionType.FILE_NOT_FOUND);
-        }catch (RuntimeException e){
+        } catch (RuntimeException e) {
             throw new CustomException(ExceptionType.INTERNAL_CSV_EXCEPTION);
         }
         return csvStateCensusData;
